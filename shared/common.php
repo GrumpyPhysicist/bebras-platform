9d09492342d385908f06e4758e06a43cf9cf7f49
Jonatan Pipping <jonatan@pipping.net>
2020-10-07 10:21:38 +0200

    * minor translation fix * removed dead code

--- shared/common.php
+++ shared/common.php
@@ -2,17 +2,6 @@
 require_once("connect.php");
 include_once("../dataSanitizer/dataSanitizer.inc.php");
 
-if (get_magic_quotes_gpc()) {
-    function stripslashes_gpc(&$value)
-    {
-        $value = stripslashes($value);
-    }
-    array_walk_recursive($_GET, 'stripslashes_gpc');
-    array_walk_recursive($_POST, 'stripslashes_gpc');
-    array_walk_recursive($_COOKIE, 'stripslashes_gpc');
-    array_walk_recursive($_REQUEST, 'stripslashes_gpc');
-}
-
 // The encoding used for multi-bytes string in always UTF-8
 mb_internal_encoding("UTF-8");
 
