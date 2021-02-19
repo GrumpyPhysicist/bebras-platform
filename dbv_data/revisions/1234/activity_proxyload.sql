997080e089a4974bd21e6472dc2544f8a77fe1c1
Jonatan Pipping <jonatan@pipping.net>
2021-02-19 18:13:33 +0100

    * fixed rev 1234

--- dbv_data/revisions/1234/activity_proxyload.sql
+++ dbv_data/revisions/1234/activity_proxyload.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `activity` CHANGE `TYPE` ENUM('load','attempt','submission','proxyload') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `activity` MODIFY `TYPE` ENUM('load','attempt','submission','proxyload') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
